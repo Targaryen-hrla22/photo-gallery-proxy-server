07fa7680d4e5b03dea3b42c2e8fd9e379f5bf956
Gus Humphrey <ghumphrey232@gmail.com>
2018-06-12 13:36:16 -0700

    [init] Server up and running

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const bodyParse = require('body-parser');
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../static')));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('Error connecting to server', err);
+  } else {
+    console.log('Listening on PORT', PORT);
+  }
+})
