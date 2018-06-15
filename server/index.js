7637e7520d8be632625d39a9301394bd4362fc1d
Gus Humphrey <ghumphrey232@gmail.com>
2018-06-15 15:09:52 -0700

    [server] Server fully functional

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
 const bodyParse = require('body-parser');
+const cors = require('cors');
 
 const PORT = 3000;
 const app = express();
+
+corsOptions = {
+  origin: "https://amazonaws.com",
+  optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: true }));
